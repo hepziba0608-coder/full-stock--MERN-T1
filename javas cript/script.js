427608037027bddd514ab3ee12e818b8fe202622
subha <hepziba0608@gmail.com>
2026-06-05 14:17:50 +0530

    JS completed

---
+++ javas cript/script.js
@@ -0,0 +1,133 @@
+///
+// var a = 10;
+
+// console.log(a--);
+// console.log(--a);
+ 
+// a = 1;
+// b = 1;
+// c = 0;
+ 
+// var result = a++ + --b +  ++c - ++a + ++b + c++ + b++ + a;
+// console.log(result)
+
+// var mark =30;
+// if(mark >= 75){
+//     console.log("A grade");
+//  }
+//  else if(mark >= 35){
+//     console.log("pass")
+//  }
+//  else{
+//     console.log("fail")
+//  }
+
+// var a = 10;
+// var result = a%2===0 ? "even": "odd";
+// // console.log(result)
+ 
+// mark = 90;
+// var result =(mark>=90) ? " O grade" :
+//             (mark>=70) ? "A grade" :
+//             (mark>=35) ? "pass" :
+//             "fail";
+// console.log(result);
+
+// var  day= 2;
+// switch(day){
+//     case 1 :{
+//         console.log("sunday");
+//         break;
+//     }
+//    case 2 :{
+//         console.log("monday");
+//          break;
+//     }
+//     case 3 :{
+//         console.log("wednesday");
+//          break;
+//     }
+//     case 4:{
+//         console.log("thursday");
+//          break;
+//     }
+//     case 5 :{
+//         console.log("friday");
+//          break;
+//     }
+//     case 6:{
+//         console.log("saturday");
+//          break;
+//     }
+//     case 7:{
+//         console.log("tuesday");
+//          break;
+//     }
+//     default :{
+//         console.log("invalid input");
+//          break;
+//     }
+// }
+
+//looping statement
+// 1 time 11 time 10 time
+
+// for(var i=1;i<=10;i++){
+//     console.log(i);
+// }
+//while loop
+
+// for(let i=1;1;=10;i++)
+// {
+//     if(i%2===0){
+//         continue;
+// }
+// console.log(i);
+// }
+// add();
+// function add(){
+//     console.log(10+20);
+// }
+// add(10+20)
+
+// function add(a=5 , b=6){
+//     console.log(a+b);
+// }
+// add();
+
+// var demo = (a=10,b=20) =>{
+//     console.log(a+b);
+
+// }
+// demo (10,20);
+// demo();
+// demo(10);
+//spread operator(...)
+
+// var arr=[10,20,30];
+// var arr2=[...arr,40,50];
+// console.log(arr);
+// console.log(arr2);
+
+// //destructuring operator
+// var [m1,m2,m3,m4,m5]=[90,99,98,97,100]
+// console.log(m1,m2,m3,m4,m5)
+
+// var {name,mobile,dept,email,isActive} = {
+//     name :"hepzi",
+//     mobile :1234567890,
+//     dept :["aids"],
+//     email:"hepziba@gmail.com",
+//     isActive:true
+// }
+// console.log(name,mobile,dept,email,isActive)
+// var obj ={
+//     name:"buela",
+//     dept:["sd"],
+//     mobile:1234567890
+
+// }
+// for(let key in obj){
+//     console.log(key,obj)
+// }
+
